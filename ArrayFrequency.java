3f73decd6743d7ed093051d84578426bbafb09b1
geeteshkumbalkar028 <geeteshkumbalkar@gmail.com>
2023-01-17 01:11:03 +0530

    Added : frequency of each element in the array

---
+++ ArrayFrequency.java
@@ -0,0 +1,34 @@
+//2) Java Program to find the frequency of each element in the array
+import java.lang.*;
+
+public class Frequency
+{
+	public int frequencyOfElement(int no)
+	{
+		
+	}
+}
+
+public class ArrayFrequency
+{	
+	public static void main(String arg[])
+	{
+		int arr[] = new int[10];  //declaration 
+		//initialization 
+		arr[0] = 10;
+		arr[1] = 20;
+		arr[2] = 30;
+		arr[3] = 40;
+		arr[4] = 30;
+		arr[5] = 60;
+		arr[6] = 30;
+		arr[7] = 40;
+		arr[8] = 50;
+		arr[9] = 60;
+		
+		for(int counter=0;counter<arr.length;counter++)
+		{
+			
+		}
+	}
+}
